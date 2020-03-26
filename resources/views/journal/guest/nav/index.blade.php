b854d141068a7ebbc32cf7d990856da15a4cc34b
Max24S <48694869+Max24S@users.noreply.github.com>
2020-03-26 19:06:11 +0300

    23

---
+++ resources/views/journal/guest/nav/index.blade.php
@@ -0,0 +1,10 @@
+#se
+<li class="nav-item active">
+    <a class="nav-link" href="#">О нас <span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="#">Преподователи</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link disabled" href="#">Обратная связь</a>
+</li>
---
+++ resources/views/journal/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+
+    <title>Carousel Template for Bootstrap</title>
+</head>
+<body>
+<script src="{{mix('js/app.js')}}"></script>
+</body>
+</html>
