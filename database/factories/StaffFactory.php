10b3a8d880c049f396c97b8783200ec587cae552
Max24S <48694869+Max24S@users.noreply.github.com>
2020-03-26 22:54:31 +0300

    Update StaffFactory.php

--- database/factories/StaffFactory.php
+++ database/factories/StaffFactory.php
@@ -8,7 +8,16 @@ use Illuminate\Support\Str;
 $factory->define(Staff::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'surname'=>$faker->name,
+        'patronymic'=>$faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        'birthday'=>$faker->dateTime,
+        'sex'=>'Мужчина',
+        'role'=>'Директор',
+        'description'=>$faker->text
+
+
+
     ];
 });
