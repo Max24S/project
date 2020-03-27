be191629d32b0e3379aaf22548dc06a8bffde15e
Ivan <54555014+StarLord2020@users.noreply.github.com>
2020-03-27 11:03:53 +0200

    g

---
+++ database/seeds/GradeClassroomSeader.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GradeClassroomSeader extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
