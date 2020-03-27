f87f1b36b079a086ccc079fde9e9668ecf1eb4b3
Ivan <54555014+StarLord2020@users.noreply.github.com>
2020-03-27 14:19:28 +0200

    Update create-user.blade.php

--- resources/views/journal/create-user.blade.php
+++ resources/views/journal/create-user.blade.php
@@ -7,254 +7,49 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
-    <style>
-        .register{
-            background: -webkit-linear-gradient(left, #3931af, #00c6ff);
-            margin-top: 3%;
-            padding: 3%;
-        }
-        .register-left{
-            text-align: center;
-            color: #fff;
-            margin-top: 4%;
-        }
-        .register-left input{
-            border: none;
-            border-radius: 1.5rem;
-            padding: 2%;
-            width: 60%;
-            background: #f8f9fa;
-            font-weight: bold;
-            color: #383d41;
-            margin-top: 30%;
-            margin-bottom: 3%;
-            cursor: pointer;
-        }
-        .register-right{
-            background: #f8f9fa;
-            border-top-left-radius: 10% 50%;
-            border-bottom-left-radius: 10% 50%;
-        }
-        .register-left img{
-            margin-top: 15%;
-            margin-bottom: 5%;
-            width: 25%;
-            -webkit-animation: mover 2s infinite  alternate;
-            animation: mover 1s infinite  alternate;
-        }
-        @-webkit-keyframes mover {
-            0% { transform: translateY(0); }
-            100% { transform: translateY(-20px); }
-        }
-        @keyframes mover {
-            0% { transform: translateY(0); }
-            100% { transform: translateY(-20px); }
-        }
-        .register-left p{
-            font-weight: lighter;
-            padding: 12%;
-            margin-top: -9%;
-        }
-        .register .register-form{
-            padding: 10%;
-            margin-top: 10%;
-        }
-        .btnRegister{
-            float: right;
-            margin-top: 10%;
-            border: none;
-            border-radius: 1.5rem;
-            padding: 2%;
-            background: #0062cc;
-            color: #fff;
-            font-weight: 600;
-            width: 50%;
-            cursor: pointer;
-        }
-        .register .nav-tabs{
-            margin-top: 3%;
-            border: none;
-            background: #0062cc;
-            border-radius: 1.5rem;
-            width: 28%;
-            float: right;
-        }
-        .register .nav-tabs .nav-link{
-            padding: 2%;
-            height: 34px;
-            font-weight: 600;
-            color: #fff;
-            border-top-right-radius: 1.5rem;
-            border-bottom-right-radius: 1.5rem;
-        }
-        .register .nav-tabs .nav-link:hover{
-            border: none;
-        }
-        .register .nav-tabs .nav-link.active{
-            width: 100px;
-            color: #0062cc;
-            border: 2px solid #0062cc;
-            border-top-left-radius: 1.5rem;
-            border-bottom-left-radius: 1.5rem;
-        }
-        .register-heading{
-            text-align: center;
-            margin-top: 8%;
-            margin-bottom: -15%;
-            color: #495057;
-        }
-    </style>
 </head>
 <body>
-<div class="container register">
-    <div class="row">
-        <div class="col-md-3 register-left">
-            <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
-            <h3>Welcome</h3>
-            <p>You are 30 seconds away from earning your own money!</p>
-            <input type="submit" name="" value="Login"/><br/>
-        </div>
-        <div class="col-md-9 register-right">
-            <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Employee</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Hirer</a>
-                </li>
-            </ul>
-            <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                    <h3 class="register-heading">Apply as a Employee</h3>
-                    <div class="row register-form">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="First Name *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Last Name *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Password *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control"  placeholder="Confirm Password *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <div class="maxl">
-                                    <label class="radio inline">
-                                        <input type="radio" name="gender" value="male" checked>
-                                        <span> Male </span>
-                                    </label>
-                                    <label class="radio inline">
-                                        <input type="radio" name="gender" value="female">
-                                        <span>Female </span>
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="email" class="form-control" placeholder="Your Email *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Your Phone *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <select class="form-control">
-                                    <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
-                                    <option>What is your Birthdate?</option>
-                                    <option>What is Your old Phone Number</option>
-                                    <option>What is your Pet Name?</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Enter Your Answer *" value="" />
-                            </div>
-                            <input type="submit" class="btnRegister"  value="Register"/>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade show" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                    <h3  class="register-heading">Apply as a Hirer</h3>
-                    <div class="row register-form">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="First Name *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Last Name *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control" placeholder="Email *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" maxlength="10" minlength="10" class="form-control" placeholder="Phone *" value="" />
-                            </div>
-
-
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Password *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Confirm Password *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <select class="form-control">
-                                    <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
-                                    <option>What is your Birthdate?</option>
-                                    <option>What is Your old Phone Number</option>
-                                    <option>What is your Pet Name?</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="`Answer *" value="" />
-                            </div>
-                            <input type="submit" class="btnRegister"  value="Register"/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 <div class="container">
     <form class="form-horizontal" role="form">
         <h2>Registration</h2>
         <div class="form-group">
-            <label for="firstName" class="col-sm-3 control-label">First Name</label>
+            <label for="firstName" class="col-sm-3 control-label">Фамилия</label>
             <div class="col-sm-9">
-                <input type="text" id="firstName" placeholder="First Name" class="form-control" autofocus>
+                <input type="text" id="firstName" placeholder="Фамилия" class="form-control" autofocus>
             </div>
         </div>
         <div class="form-group">
-            <label for="lastName" class="col-sm-3 control-label">Last Name</label>
+            <label for="midlefame" class="col-sm-3 control-label">Имя</label>
             <div class="col-sm-9">
-                <input type="text" id="lastName" placeholder="Last Name" class="form-control" autofocus>
+                <input type="text" id="firstName" placeholder="Имя" class="form-control" autofocus>
             </div>
         </div>
         <div class="form-group">
-            <label for="email" class="col-sm-3 control-label">Email* </label>
+            <label for="lastName" class="col-sm-3 control-label">Отчество</label>
+            <div class="col-sm-9">
+                <input type="text" id="lastName" placeholder="Отчество" class="form-control" autofocus>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="email" class="col-sm-3 control-label">Email </label>
             <div class="col-sm-9">
                 <input type="email" id="email" placeholder="Email" class="form-control" name= "email">
             </div>
         </div>
         <div class="form-group">
-            <label for="password" class="col-sm-3 control-label">Password*</label>
+            <label for="password" class="col-sm-3 control-label">Password</label>
             <div class="col-sm-9">
                 <input type="password" id="password" placeholder="Password" class="form-control">
             </div>
         </div>
         <div class="form-group">
-            <label for="password" class="col-sm-3 control-label">Confirm Password*</label>
+            <label for="password" class="col-sm-3 control-label">Confirm Password</label>
             <div class="col-sm-9">
                 <input type="password" id="password" placeholder="Password" class="form-control">
             </div>
         </div>
         <div class="form-group">
-            <label for="birthDate" class="col-sm-3 control-label">Date of Birth*</label>
+            <label for="birthDate" class="col-sm-3 control-label">Date of Birth</label>
             <div class="col-sm-9">
                 <input type="date" id="birthDate" class="form-control">
             </div>
@@ -267,15 +62,9 @@
             </div>
         </div>
         <div class="form-group">
-            <label for="Height" class="col-sm-3 control-label">Height* </label>
-            <div class="col-sm-9">
-                <input type="number" id="height" placeholder="Please write your height in centimetres" class="form-control">
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="weight" class="col-sm-3 control-label">Weight* </label>
-            <div class="col-sm-9">
-                <input type="number" id="weight" placeholder="Please write your weight in kilograms" class="form-control">
+            <label class="col-md-4 control-label" for="address">Address</label>
+            <div class="col-md-4">
+                <textarea class="form-control" id="address" name="address">default text</textarea>
             </div>
         </div>
         <div class="form-group">
@@ -284,22 +73,17 @@
                 <div class="row">
                     <div class="col-sm-4">
                         <label class="radio-inline">
-                            <input type="radio" id="femaleRadio" value="Female">Female
+                            <input name='sex' type="radio" id="femaleRadio" value="Female">Female
                         </label>
                     </div>
                     <div class="col-sm-4">
                         <label class="radio-inline">
-                            <input type="radio" id="maleRadio" value="Male">Male
+                            <input name='sex' type="radio" id="maleRadio" value="Male">Male
                         </label>
                     </div>
                 </div>
             </div>
         </div> <!-- /.form-group -->
-        <div class="form-group">
-            <div class="col-sm-9 col-sm-offset-3">
-                <span class="help-block">*Required fields</span>
-            </div>
-        </div>
         <button type="submit" class="btn btn-primary btn-block">Register</button>
     </form> <!-- /form -->
 </div> <!-- ./container -->
