8a61082af9c43cdf797645018c0cc057e4a59df1
Max24S <48694869+Max24S@users.noreply.github.com>
2020-03-29 17:36:55 +0300

    Update create.blade.php

--- resources/views/admin/teacher/homework/create.blade.php
+++ resources/views/admin/teacher/homework/create.blade.php
@@ -1,45 +1,42 @@
 @extends('admin.teacher.layouts.teacher')
 @section('form')
+    @php
+        $class='10-A';
+        $date='100' ;
+    @endphp
     <form action="">
-        <table class="table">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Предмет</th>
-                <th scope="col">Преподователь</th>
-                <th scope="col">
-                    <select name="" id="">
-                        <option >Выберите день недели</option>
-                        <option value="day">Понедельник</option>
-                    </select>
-                </th>
-                <th scope="col">
-                    <select name="" id="">
-                        <option>Выберите класс</option>
-                        <option value="day">1-А</option>
-                    </select>
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            @for($i=1;$i<9;$i++)
-                <tr>
-                    <th scope="row">{{$i}}</th>
-                    <td>
-                        <select name="" id="">
-                            <option value="day">-</option>
-                            <option value="day">Математика</option>
-                        </select>
-                    </td>
-                    <td>
-                        <select name="" id="">
-                            <option value="day">-</option>
-                            <option value="day">Полох И.В.</option>
-                        </select>
-                    </td>
-                </tr>
-            @endfor
-            </tbody>
-        </table>
-        <button type="submit">Cохранить</button>
+       <div class="information">
+           Класс
+           <select name="class" id="">
+               <option disabled value="">
+                   {{$class}}
+               </option>
+               <option value="">10-A</option>
+           </select>
+           Дата домашней работы
+           <select name="" id="">
+               <option disabled value="">
+                   {{$date}}
+               </option>
+               <option value="">10-A-{{$class}}</option>
+           </select>
+       </div>
+        <div>
+            Задание
+            <input type="text" name="name" id="" >
+            Комментарии
+            <textarea name="description" id="" cols="30" rows="10" ></textarea>
+            Статус
+            <select name="`status" id="">
+                <option value="">Выдано</option>
+            </select>
+        </div>
+
+        <button value="edit" type="submit">Изменить</button>
+        <button value="save" type="submit">Cохранить</button>
     </form>
+    <style>
+        textarea {
+            resize: none !important; /* Запрещаем изменять размер */
+        }
+    </style>
