502b7ac48c3541ccfda1b6f9bfede2fc64db77bf
Ivan <54555014+StarLord2020@users.noreply.github.com>
2020-03-30 15:55:53 +0300

    d

--- resources/views/admin/teacher/head-teacher/layouts/blocks/nav/index.blade.php
+++ resources/views/admin/teacher/head-teacher/layouts/blocks/nav/index.blade.php
@@ -4,22 +4,9 @@
         <div class="btn-group">
             <button type="button" class="btn btn-1  dropdown-toggle text-white" data-toggle="dropdown" >Расписание</button>
             <div class="dropdown-menu">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                <a class="dropdown-item" href="#">Создать</a>
-=======
                 <a class="dropdown-item" href="{{route('admin.teacher.head-teacher.timetable.create')}}">Создать</a>
->>>>>>> 68647d690d560069f6a19905c1d120c29d2882e4
-=======
-                <a class="dropdown-item" href="{{route('admin.teacher.head-teacher.timetable.create')}}">Создать</a>
->>>>>>> 13ed31e956dc045c9d9d92b2810251e0a8698776
-=======
-                <a class="dropdown-item" href="{{route('admin.teacher.head-teacher.timetable.create')}}">Создать</a>
->>>>>>> 13ed31e956dc045c9d9d92b2810251e0a8698776
                 <a class="dropdown-item" href="#">Редактировать</a>
             </div>
         </div>
     </li>
 @endsection
-
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-Route::view('/', 'admin.teacher.head-teacher.timetable.create')->name('welcome');
-=======
 Route::view('/', 'admin.teacher.head-teacher.index')->name('welcome');
->>>>>>> 68647d690d560069f6a19905c1d120c29d2882e4
-=======
-цуй
-Route::view('/', 'admin.teacher.head-teacher.index')->name('welcome');
->>>>>>> 13ed31e956dc045c9d9d92b2810251e0a8698776
-=======
-цуй
-Route::view('/', 'admin.teacher.head-teacher.index')->name('welcome');
->>>>>>> 13ed31e956dc045c9d9d92b2810251e0a8698776
 Route::name('admin.')
     ->namespace('Admin')
     ->prefix('admin')
@@ -27,36 +12,11 @@ Route::name('admin.')
             ->name('super.')
             ->namespace('Super')
             ->group(function () {
-                Route::view('/','admin.super.index');
                 Route::resource('classroom', 'ClassroomController');
                 Route::resource('grade', 'GradeController');
                 Route::resource('subject', 'SubjectController');
                 Route::resource('student', 'StudentController');
                 Route::resource('staff', 'StaffController');
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-            });
-
-
-        Route::view('/', 'admin.teacher.index');
-
-//        Route::prefix('super')
-//            ->name('super.')
-//            ->namespace('Super')
-//            ->group(function () {
-//                Route::resource('/', 'ClassroomController');
-//            });
-=======
->>>>>>> b4d31f5182d991f0b0abff4c6bee60a51c0b4f26
-=======
->>>>>>> 68647d690d560069f6a19905c1d120c29d2882e4
-=======
->>>>>>> 13ed31e956dc045c9d9d92b2810251e0a8698776
-=======
->>>>>>> 13ed31e956dc045c9d9d92b2810251e0a8698776
 
             });
         Route::prefix('teacher')
@@ -65,11 +25,11 @@ Route::name('admin.')
             ->group(function () {
                 Route::resource('homework', 'HomeworkController');
                 Route::prefix('head-teacher')
-                ->namespace('HeadTeacher')
-                ->name('head-teacher.')
-                ->group(function () {
-                    Route::resource('timetable', 'TimetableController');
-                });
+                    ->namespace('HeadTeacher')
+                    ->name('head-teacher.')
+                    ->group(function () {
+                        Route::resource('timetable', 'TimetableController');
+                    });
             });
     });
 
