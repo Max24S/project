bd41860b4fc6501fa5a464dadf79d14ba83d4c6c
Ivan <54555014+StarLord2020@users.noreply.github.com>
2020-04-02 02:43:55 +0300

    r

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/1', 'Mail\MailController@send');
 Route::view('/', 'admin.teacher.head-teacher.index')->name('welcome');
 Route::name('admin.')
     ->namespace('Admin')
