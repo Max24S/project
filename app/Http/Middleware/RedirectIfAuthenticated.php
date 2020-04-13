7e160d926965217b9dec935c051559f4d018a5a7
Max24S <48694869+Max24S@users.noreply.github.com>
2020-04-13 22:00:55 +0300

    321321312321

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,19 +18,6 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        $user = Auth::user();
-        if(isset($user->role)) {
-            switch ($user->role) {
-                case'Ученик':
-                    return redirect(RouteServiceProvider::STUDENT);
-                case'Учитель':
-                    return redirect(RouteServiceProvider::STUDENT);
-                case'Завуч':
-                    return redirect(RouteServiceProvider::STUDENT);
-                case'Админ':
-                    return redirect(RouteServiceProvider::STUDENT);
-            }
-        }
         if (Auth::guard($guard)->check()) {
             return redirect(RouteServiceProvider::HOME);
         }
