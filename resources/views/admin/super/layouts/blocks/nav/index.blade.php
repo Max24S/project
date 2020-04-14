ce3dc1f37c19d34149e50218af041ac65001227c
Ivan <54555014+StarLord2020@users.noreply.github.com>
2020-04-15 00:56:07 +0300

    d

--- resources/views/admin/super/layouts/blocks/nav/index.blade.php
+++ resources/views/admin/super/layouts/blocks/nav/index.blade.php
@@ -1,5 +1,8 @@
 <style>
      a {
+         color: #fff;
+    }
+    a:hover {
         color: #fff;
     }
 </style>
@@ -33,21 +36,21 @@
                     <div>
                         @guest
                         @else
-                            <span class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-                        <div class="dropdown-menu position-absolute" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                               document.getElementById('logout-form').submit();">
-                                {{ __('Выйти') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                    </div>
-                </span>
+                            <div class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu position-absolute" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                                    {{ __('Выйти') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                             </div>
                         @endguest
                     </div>
                 </div>
